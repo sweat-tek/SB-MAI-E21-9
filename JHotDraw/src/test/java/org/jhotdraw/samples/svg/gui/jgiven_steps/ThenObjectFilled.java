827441abaaafea8e069ede1d91407f32fcb4141b
Yusaf Tareen <gayssuck123@gmail.com>
2021-12-05 22:45:55 +0100

    Working on BDD-Test for Fill Palette

--- JHotDraw/src/test/java/org/jhotdraw/samples/svg/gui/jgiven_steps/ThenObjectFilled.java
+++ JHotDraw/src/test/java/org/jhotdraw/samples/svg/gui/jgiven_steps/ThenObjectFilled.java
@@ -3,7 +3,6 @@ package org.jhotdraw.samples.svg.gui.jgiven_steps;
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import org.jhotdraw.draw.DrawingEditor;
-import org.jhotdraw.draw.Figure;
 
 import static org.junit.Assert.assertEquals;
 
@@ -12,13 +11,12 @@ public class ThenObjectFilled extends Stage<ThenObjectFilled> {
     @ExpectedScenarioState
     private DrawingEditor editor;
 
-    @ExpectedScenarioState
-    private Figure f1;
-
     public ThenObjectFilled the_object_is_filled_with_a_default_color() {
 
         // the object was selected and thus filled with a default color (white)
         assertEquals(editor.getActiveView().getSelectionCount(), 1);
+        
+        // todo: assert that object has color white
 
         return self();
     }
--- JHotDraw/src/test/java/org/jhotdraw/samples/svg/gui/jgiven_steps/WhenObjectSelected.java
+++ JHotDraw/src/test/java/org/jhotdraw/samples/svg/gui/jgiven_steps/WhenObjectSelected.java
@@ -4,57 +4,44 @@ import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.BeforeStage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
-import org.jhotdraw.app.action.SelectAllAction;
-import org.jhotdraw.draw.DrawingEditor;
-import org.jhotdraw.draw.Figure;
+import org.jhotdraw.draw.*;
 import org.jhotdraw.samples.svg.gui.FillToolBar;
 
 import javax.swing.*;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotEquals;
 
 public class WhenObjectSelected extends Stage<WhenObjectSelected> {
 
     @ExpectedScenarioState
-    @ProvidedScenarioState
-    private DrawingEditor editor;
+    private DefaultDrawingView view;
 
-    @ProvidedScenarioState
-    private Set<Figure> selectedFiguresOnView;
-
-    private SelectAllAction selectAction;
-
+    /* // Disabled for now - IDE error
     @ProvidedScenarioState
     private FillToolBar bar;
 
     @ProvidedScenarioState
     private JComponent disclosedComponent;
 
-    @ProvidedScenarioState
-    private Figure f1;
-
     @BeforeStage
     private void before() {
-        selectAction = new SelectAllAction();
         bar = new FillToolBar();
-
-        selectedFiguresOnView = new HashSet<>(editor.getActiveView().getSelectedFigures());
     }
+    */
 
     public WhenObjectSelected an_object_is_selected() {
-        // checks that figure was selected
-        assertTrue(editor.getActiveView().isFigureSelected(f1));
+        // select all objects
+        view.selectAll();
+        
+        // checks that objects were selected
+        assertNotEquals(0, view.getSelectionCount());
 
         // this should now be the case:
-        // disclosedComponent = bar.createDisclosedComponent(2);
+        //disclosedComponent = bar.createDisclosedComponent(2);
 
         // check that the fill palette tool is open
         // if it is open --> then default paint color is white
-        assertEquals(3, disclosedComponent.getComponentCount());
+        //assertEquals(3, disclosedComponent.getComponentCount());
 
         return self();
     }
